c7c5bb2c63749a55af5f39034ca73936d01fd8ce
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-05-16 23:19:33 +0200

    feat(home): added email

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,9 @@
   <div class="hello">
     <h1>Binnenkort meer...</h1>
     <p>
-      Ontdek meer op <a href="https://chianederland.nl/discord" target="_blank" rel="noopener">onze Discord server</a>
+      Ontdek meer op <a href="https://chianederland.nl/discord" target="_blank" rel="noopener">onze Discord server</a>,
+      <br/>
+      of stuur een email naar <a href="mailto:hello@chianederland.nl">hello@chianederland.nl</a> :)
     </p>
   </div>
 </template>
