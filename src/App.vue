ff5d20168cda10670b14a52c4f320cee1cbb5d76
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-05-16 23:07:30 +0200

    init: added landingpage

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="ChiaNederland logo" src="./assets/logo.png" width="350">
+  <Home/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+html, body {
+  padding: 0;
+  margin: 0;
+  background: #ffffff;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>Binnenkort meer...</h1>
+    <p>
+      Ontdek meer op <a href="https://chianederland.nl/discord" target="_blank" rel="noopener">onze Discord server</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+a {
+  color: #ff931e;
+}
+</style>
